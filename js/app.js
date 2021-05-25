aa9bae6d6355ccacbc55ae1fcef872a56595ba9a
bushraaljafari <bushra.aljafari@gmail.com>
2021-05-25 13:23:46 +0300

    get eslint active

--- js/app.js
+++ js/app.js
@@ -3,111 +3,111 @@
 
 alert('welcome in my page');
 
-let name = prompt('what is your name ?');
-console.log(name);
-alert('hi,' + name);
+let namea = prompt('what is your name ?');
+console.log(namea);
+alert('hi,' + namea);
 
 alert('we will play aguessing game about me lets go');
 
 let go = prompt('my name is bushra!');
 //console.log(go);
 switch (go.toLowerCase()) {
-    case ' yes':
-    case 'y':
-        alert('corect bushra is my name');
+case ' yes':
+case 'y':
+  alert('corect bushra is my name');
 
-        break;
-    case 'no':
-    case 'n':
-        alert('wrong my name is bushra');
-break;
-    default:
-        alert('please answer with yes or no to qustions');
-        break;
+  break;
+case 'no':
+case 'n':
+  alert('wrong my name is bushra');
+  break;
+default:
+  alert('please answer with yes or no to qustions');
+  break;
 }
 
 let age = prompt('my age is 24!');
 //console.log(age);
 switch (age.toLowerCase()) {
-    case ' yes':
-    case 'y':
-        alert('correct my age is 24');
+case ' yes':
+case 'y':
+  alert('correct my age is 24');
 
-        break;
-    case 'no':
-    case 'n':
-        alert('sorry i am 24');
-        break;
-    default:
-        alert('please answer with yes or no to qustions');
-        break;
+  break;
+case 'no':
+case 'n':
+  alert('sorry i am 24');
+  break;
+default:
+  alert('please answer with yes or no to qustions');
+  break;
 }
 
 let favouritColor = prompt('my favourite color is blue!');
 //console.log(favouritColor);
 switch (favouritColor.toLowerCase()) {
-    case ' yes':
-    case 'y':
-        alert('no,it is red');
+case ' yes':
+case 'y':
+  alert('no,it is red');
 
-        break;
-    case 'no':
-    case 'n':
-        alert('you right ,it is red');
-break;
-    default:
-        alert('please answer with yes or no to qustions');
-        break;
+  break;
+case 'no':
+case 'n':
+  alert('you right ,it is red');
+  break;
+default:
+  alert('please answer with yes or no to qustions');
+  break;
 }
 
 let study = prompt('i have an abachoular degree in civil engineer');
 //console.log(study);
 switch (study.toLowerCase()) {
-    case ' yes':
-    case 'y':
-        alert('yes i have it ');
+case ' yes':
+case 'y':
+  alert('yes i have it ');
 
-        break;
-    case 'no':
-    case 'n':
-        alert('i am actually have it');
-break;
-    default:
-        alert('please answer with yes or no to qustions');
-        break;
+  break;
+case 'no':
+case 'n':
+  alert('i am actually have it');
+  break;
+default:
+  alert('please answer with yes or no to qustions');
+  break;
 }
 let travel = prompt('i like to travel ');
 //console.log(travel);
 switch (travel.toLowerCase()) {
-    case ' yes':
-    case 'y':
-        alert('of course yes ');
+case ' yes':
+case 'y':
+  alert('of course yes ');
 
-        break;
-    case 'no':
-    case 'n':
-        alert('what who doesnt like travel!');
-break;
-    default:
-        alert('please answer with yes or no to qustions');
-        break;
+  break;
+case 'no':
+case 'n':
+  alert('what who doesnt like travel!');
+  break;
+default:
+  alert('please answer with yes or no to qustions');
+  break;
 }
 
 let game=prompt('i am agamer ');
 //console.log(game);
 switch (game.toLowerCase()) {
-    case' yes':
-        case 'y':
-            alert('no i am not agamer ');
-        
-        break;
-        case 'no':
-            case 'n':
-                alert('you are right');
- break;
-    default:
-        alert('please answer with yes or no to qustions');
-        break;
+case' yes':
+case 'y':
+  alert('no i am not agamer ');
+
+  break;
+case 'no':
+case 'n':
+  alert('you are right');
+  break;
+default:
+  alert('please answer with yes or no to qustions');
+  break;
 }
 /*let game = prompt('i am agamer ');
 //console.log(game);
